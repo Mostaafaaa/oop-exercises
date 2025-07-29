abf2118ac1ad14b88d0fb078bded35a5282e7ea3
Zahraa <shugo7385@gmail.com>
2025-07-29 18:19:35 +0300

    first set

---
+++ ZahraaOOP_Excercises/Rectangle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZahraaOOP_Excercises
+{
+    /*Exercise 3: Create a Rectangle class with attributes width and height. 
+     * Add methods area() and perimeter() to calculate and return the values.*/
+    internal class Rectangle
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public double Area()
+        {
+            return Width * Height;
+        }
+
+        public double Perimeter()
+        {
+            return 2 * (Width + Height);
+        }
+    }
+}
---
+++ ZahraaOOP_Excercises/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZahraaOOP_Excercises
+{
+    /*Exercise 2: Create a Student class with attributes name, age, and grade. 
+     * Add a method introduce() that prints the student details.*/
+    internal class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Grade { get; set; }
+        public Student(string name, int age, string grade)
+        {
+            Name = name;
+            Age = age;
+            Grade = grade;
+        }
+        public void Introduce()
+        {
+            Console.WriteLine($"{Name}'s age is: {Age} years old and the grade : {Grade}.");
+        }
+    }
+}
